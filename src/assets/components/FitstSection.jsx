acfaab8a7f982a9db7096266af6ffbb0148f4d6c
nihalmohd <nihalmohd828@gmail.com>
2024-10-31 18:36:05 +0530

    Nav bar is over

---
+++ src/assets/components/FitstSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FitstSection = () => {
+  return (
+    <div>FitstSection</div>
+  )
+}
+
+export default FitstSection
--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -4,37 +4,47 @@ const Nav = () => {
   return (
     <div>
       <div className="w-full h-16 shadow-md flex justify-center items-center">
-        <div className="w-10/12 h-12 bg-green-200 flex justify-between items-center gap-1">
-          <div className="w-3/12 h-full bg-yellow-200 flex justify-end ">
-            <div className="w-[60px] h-full bg-red-200 flex justify-end items-center">
+        <div className="w-10/12 h-12  flex justify-between items-center gap-1">
+          <div className="w-3/12 h-full  flex justify-end items-end  ">
+            <div className="w-[55px] h-[41px]  flex justify-end items-end">
               <img src="\images\ab6aaab828b7c3f0ef708693166c9def.png" className='w-full h-full ' alt="" />
             </div>
-            <div className="w-80 h-full bg-red-200">
-              <div className="w-full h-full bg-gray-600 flex items-center">
+            <div className="w-80 h-full ">
+              <div className="w-full h-full  flex items-center">
                 
-                <h1 className='font-nexa text-[30px] text-[#5B297E] font-medium'>LoaDart</h1>
+                <h1 className='font-nexa text-[23px] text-[#5B297E] font-semibold'>LoaDart</h1>
               </div>
 
             </div>
           </div>
-          <div className="w-9/12 h-full bg-yellow-200 flex justify-end items-center">
-             <div className="w-9/12 h-12 bg-green-500 flex gap-3 items-center">
-                <div className="w-2/12 h-10 bg-red-200 flex justify-center items-center ">
-                  <h1 className='text-md font-semibold text-black'>About</h1>
+          <div className="w-9/12 h-full  flex justify-end items-center">
+             <div className="w-10/12 h-8  flex gap-4 items-center ">
+                <div className="w-1/12 h-10  flex justify-start items-center ">
+                  <h1 className='text-[13px]  font-inter font-medium text-black'>About</h1>
                 </div>
-                <div className="w-2/12 h-10 bg-red-200 flex justify-center items-center ">
-                  <h1 className='text-md font-semibold text-black'>Marketplace</h1>
+                <div className="w-2/12 h-10  flex justify-start items-center ">
+                  <h1 className='text-[13px]  font-inter  text-black'>Marketplace</h1>
                 </div>
-                <div className="w-2/12 h-10 bg-red-200 flex justify-center items-center ">
-                  <h1 className='text-md font-semibold text-black'>contact</h1>
+                <div className="w-1/12 h-10  flex justify-center items-center ">
+                  <h1 className='text-[13px]  font-inter  text-black'>Contact</h1>
                 </div>
-                <div className="w-5/12 h-10 bg-red-200"></div>
-                <button className="w-2/12 h-10 bg-red-200"></button>
+                <div className="w-6/12 h-10 flex gap-1">
+                 <div className="w-1/2 h-full flex justify-center items-center">
+                  <img src="\images\Screenshot 2024-10-31 002925.png" className='w-fit h-fit' alt="" />
+                 </div>
+                 <div className="w-1/2 h-full flex justify-center items-center">
+                  <img src="\images\Screenshot 2024-10-31 001830.png" className='w-fit h-fit' alt="" />
+                 </div>
+                </div>
+                <button className="w-2/12 h-10 bg-[#5B297E] rounded-md">
+                <h1 className='text-[13px]  font-inter  text-white '>Sign In </h1>
+                </button>
                 
              </div>
           </div>
         </div>
       </div>
+      
     </div>
   )
 }
