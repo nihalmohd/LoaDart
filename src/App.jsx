3bcd1d474bd70746685050702eb790c4df6faf7e
nihalmohd <nihalmohd828@gmail.com>
2024-11-05 14:27:08 +0530

    signup page started

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Home from './assets/pages/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Transpoter from './assets/components/Signup/Transpoter';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/" element={<Home />} />
       </Routes>
     </Router>
---
+++ src/assets/components/Signup/Transpoter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Transpoter = () => {
+  return (
+    <div>Transpoter</div>
+  )
+}
+
+export default Transpoter
