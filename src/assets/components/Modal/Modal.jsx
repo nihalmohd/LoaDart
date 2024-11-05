b6c15efa38b6b99ffd81f08f4cfabda13db040af
nihalmohd <nihalmohd828@gmail.com>
2024-11-05 18:07:07 +0530

    shipper sign up page is over

--- src/assets/components/Modal/Modal.jsx
+++ src/assets/components/Modal/Modal.jsx
@@ -2,17 +2,20 @@
 import React from 'react';
 
 const Modal = ({ isOpen, closeModal, children }) => {
-  if (!isOpen) return null; 
+    if (!isOpen) return null;
 
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white w-96 p-5 rounded-lg shadow-lg relative">
-        <button className="absolute top-2 right-2 text-red-500" onClick={closeModal}>X</button>
-         <children/>
-        {children || <p>No content available</p>}
-      </div>
-    </div>
-  );
+    return (
+        <div className="fixed inset-0 w-full h-screen bg-black bg-opacity-85 flex justify-center items-center z-50  "
+         onClick={closeModal}
+         >
+            <div 
+                className="relative"
+                onClick={(event) => event.stopPropagation()}
+            >
+                {children || <p>No content available</p>}
+            </div>
+        </div>
+    );
 };
 
 export default Modal;
