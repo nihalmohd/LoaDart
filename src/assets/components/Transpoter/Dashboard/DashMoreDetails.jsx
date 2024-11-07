3e9ae62d7fee7eb7065f5698a3b340e849779dba
nihalmohd <nihalmohd828@gmail.com>
2024-11-07 10:42:32 +0530

    picture bulging fixed

--- src/assets/components/Transpoter/Dashboard/DashMoreDetails.jsx
+++ src/assets/components/Transpoter/Dashboard/DashMoreDetails.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const DashMoreDetails = () => {
   return (
     <div>
-        <div className="w-full h-64  mt-2 flex justify-center items-center p-2">
-            <div className="w-full h-60 rounded-md border border-black flex">
+        <div className="w-full h-60  mt-2 flex justify-center items-center p-2">
+            <div className="w-full h-52 rounded-md border border-black flex">
                 <div className="w-3/12 h-full ">
                   <div className="w-full h-1/2 flex items-end pl-4">
                     <h1 className='font-inter text-5xl font-extrabold text-[#5B297E]'>₹4500</h1>
@@ -17,8 +17,8 @@ const DashMoreDetails = () => {
                   </div>
                 </div>
 
-                <div className="w-4/12 h-full ">
-                   <img src="\images\a4971eec5d96cf72041a5d776efe6b97.png" className='w-full h-full object-cover' alt="" />
+                <div className="w-3/12 h-full ">
+                   <img src="\images\a4971eec5d96cf72041a5d776efe6b97.png" className='object-cover w-full h-full overflow-hidden' alt="" />
                 </div>
             </div>
         </div>
