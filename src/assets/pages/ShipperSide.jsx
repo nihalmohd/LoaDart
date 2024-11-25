36b2fbafbd3e6d05e3b380442082bcb2bb15560c
nihalmohd <nihalmohd828@gmail.com>
2024-11-25 12:11:13 +0530

    is the dashboard is over

--- src/assets/pages/ShipperSide.jsx
+++ src/assets/pages/ShipperSide.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
 import Sidebar from '../components/Shipper/SideBar/Sidebar'
+import TopButtons from '../components/Shipper/Dashboard/TopButtons'
 
 const ShipperSide = () => {
   return (
     <div className='md:flex'>
      <Sidebar/>
+     <div className="flex-1" >
+     <Routes>
+     <Route path="/" element={<TopButtons/>} />
+     </Routes>
+
+     </div>
     </div>
   )
 }
