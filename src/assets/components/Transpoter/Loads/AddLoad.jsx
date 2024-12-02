5a250153a17ae230655b1ad7844bc3a623280321
nihalmohd <nihalmohd828@gmail.com>
2024-12-02 17:51:11 +0530

    is the your load page updated button

--- src/assets/components/Transpoter/Loads/AddLoad.jsx
+++ src/assets/components/Transpoter/Loads/AddLoad.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { IoCubeOutline } from "react-icons/io5";
+import { useNavigate } from 'react-router-dom';
 
 
 const AddLoad = () => {
@@ -7,13 +8,14 @@ const AddLoad = () => {
     const handleTruckChange = (value) => {
       setNumTrucks((prev) => Math.max(1, prev + value));
     };
+    const navigate = useNavigate()
   return (
     <div className='w-full '>
        <div className="w-11/12 h-full ">
         <div className="w-full  h-16 p-3 ">
           <div className="w-full h-10 flex justify-between items-end ml-4">
             <h1 className="font-inter font-semibold text-[#5B297E] text-lg mt-5">Add Load</h1>
-            <button className="w-28 h-6 bg-[#5B297E] flex justify-center items-center gap-1 rounded-sm text-white text-xs mb-2"><IoCubeOutline className='font-semibold'/>Your Loads</button>
+            <button className="w-28 h-6 bg-[#5B297E] flex justify-center items-center gap-1 rounded-sm text-white text-xs mb-2"onClick={()=>{navigate('/Transpoter/YourLoads')}}><IoCubeOutline className='font-semibold'/>Your Loads</button>
           </div>
         </div>
       <div className="w-full h-screen  p-2">
