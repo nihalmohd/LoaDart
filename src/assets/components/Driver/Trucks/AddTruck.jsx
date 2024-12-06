d6549245ce6d4a6cc5d25486a3fcc612fd61e103
nihalmohd <nihalmohd828@gmail.com>
2024-12-06 16:34:02 +0530

    all side add truck page is over

--- src/assets/components/Driver/Trucks/AddTruck.jsx
+++ src/assets/components/Driver/Trucks/AddTruck.jsx
@@ -221,40 +221,46 @@ const AddTruck = () => {
           </div>
           
         </div>
-        <label className="text-xs font-medium  mb-2">Prefered Locations<span className='text-red-600'>*</span></label>
-          <div className="md:w-8/12 h-auto  border-b border-gray-300 flex justify-between gap-1 p-1">
-            <div className="w-11/12 h-10  overflow-x-auto grid grid-flow-col gap-2">
-            <div className="w-auto h-full bg-[#F5F4F4] border border-[#5B297E] rounded-full flex justify-center items-center gap-1">
-              <div className="w-auto h-6 flex justify-center items-center ">
-                <h1 className='text-sm font- font-inter text-[#5B297E]'>Kozhikode</h1>
-              </div>
-              <div className="w-auto h-6  flex justify-center items-center text-red-600">
-              <IoClose />
-              </div>
-            </div>
-            <div className="w-auto h-full bg-[#F5F4F4] border border-[#5B297E] rounded-full flex justify-center items-center gap-1">
-              <div className="w-auto h-6 flex justify-center items-center">
-                <h1 className='text-sm font- font-inter text-[#5B297E]'>Malappram</h1>
-              </div>
-              <div className="w-auto h-6  flex justify-center items-center text-red-600">
-              <IoClose />
-              </div>
-            </div>
-             <div className="w-auto h-full bg-[#F5F4F4] border border-[#5B297E] rounded-full flex justify-center items-center gap-1">
-              <div className="w-auto h-6 flex justify-center items-center">
-                <h1 className='text-sm font- font-inter text-[#5B297E]'>Thrissur</h1>
-              </div>
-              <div className="w-auto h-6  flex justify-center items-center text-red-600">
-              <IoClose />
-              </div>
-            </div>
-            
-            </div>
-            <div className="w-auto h-full  flex justify-center items-center">
-              <h1 className='text-xl text-[#5B297E]'><IoIosSearch /></h1>
-            </div>
-          
-          </div>
+        <div className=" flex flex-col">
+                            <label className="text-xs font-medium text-gray-400 mb-2">
+                                Prefered Location<span className="text-red-600">*</span>
+                            </label>
+                            <select className="md:w-8/12  h-10 border-b border-gray-300   text-black focus:outline-none">
+                            <option value="Baksa">Select Location</option>
+                                <option value="Baksa">Baksa</option>
+                                <option value="Barpeta">Barpeta</option>
+                                <option value="Biswanath">Biswanath</option>
+                                <option value="Bongaigaon">Bongaigaon</option>
+                                <option value="Cachar">Cachar</option>
+                                <option value="Charaideo">Charaideo</option>
+                                <option value="Chirang">Chirang</option>
+                                <option value="Darrang">Darrang</option>
+                                <option value="Dhemaji">Dhemaji</option>
+                                <option value="Dhubri">Dhubri</option>
+                                <option value="Dibrugarh">Dibrugarh</option>
+                                <option value="Goalpara">Goalpara</option>
+                                <option value="Golaghat">Golaghat</option>
+                                <option value="Hailakandi">Hailakandi</option>
+                                <option value="Hojai">Hojai</option>
+                                <option value="Jorhat">Jorhat</option>
+                                <option value="Kamrup">Kamrup</option>
+                                <option value="Kamrup Metropolitan">Kamrup Metropolitan</option>
+                                <option value="Karbi Anglong">Karbi Anglong</option>
+                                <option value="Karimganj">Karimganj</option>
+                                <option value="Kokrajhar">Kokrajhar</option>
+                                <option value="Lakhimpur">Lakhimpur</option>
+                                <option value="Majuli">Majuli</option>
+                                <option value="Morigaon">Morigaon</option>
+                                <option value="Nagaon">Nagaon</option>
+                                <option value="Nalbari">Nalbari</option>
+                                <option value="Sivasagar">Sivasagar</option>
+                                <option value="Sonitpur">Sonitpur</option>
+                                <option value="South Salmara-Mankachar">South Salmara-Mankachar</option>
+                                <option value="Tinsukia">Tinsukia</option>
+                                <option value="Udalguri">Udalguri</option>
+                                <option value="West Karbi Anglong">West Karbi Anglong</option>
+                            </select>
+                        </div>
 
           <button className=' w-4/12 md:w-2/12  h-10 mt-3 md:mt-0  border border-[#5B297E] text-white bg-[#5B297E] rounded-sm font-inter flex justify-center items-center text-sm   shadow-md'>Proceed</button>
 
